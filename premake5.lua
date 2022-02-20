cb4b65f63f09a7edabf63697d248a77b221853bb
r083r70 <roberto.baldi@mail.polimi.it>
2022-02-20 10:38:05 +0100

    First commit

---
+++ premake5.lua
@@ -0,0 +1,32 @@
+workspace "coffeepot"
+    architecture "x86_64"
+    configurations { "Debug", "Release" }
+    startproject "coffeepot"
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "coffeepot"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "c++17"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+    files { "src/**.h", "src/**.cpp" }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "system:linux"
+        systemversion "latest"
+        links { "pthread", "dl" }
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
+
