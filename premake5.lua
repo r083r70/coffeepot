aa834d128a8f10a3c50bc2dc3ade22af2ba95e11
r083r70 <roberto.baldi@mail.polimi.it>
2022-10-25 00:14:43 +0200

    New project structure, dependent on Kettle

--- premake5.lua
+++ premake5.lua
@@ -1,77 +1,57 @@
-require "vscode"
-
 workspace "coffeepot"
-    architecture "x86_64"
-    configurations { "Debug", "Release" }
-    startproject "coffeepot"
+	architecture "x86_64"
+	configurations { "Debug", "Release" }
+	startproject "coffeepot"
 
-outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-
-include "thirdparty/glfw"
-include "thirdparty/glad"
-include "thirdparty/imgui"
+include "kettle/kettleCore"
 include "thirdparty/yaml-cpp"
 
 project "coffeepot"
-    kind "ConsoleApp"
+	kind "ConsoleApp"
 
-    language "C++"
-    cppdialect "C++17"
+	language "C++"
+	cppdialect "C++17"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("obj/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/%{cfg.buildcfg}")
+	objdir ("obj/%{cfg.buildcfg}" )
 
-    includedirs {
-        "res/",
-        "src/",
-        "thirdparty/glad/include",
-        "thirdparty/glfw/include",
-        "thirdparty/imgui",
-        "thirdparty/spdlog/include",
-        "thirdparty/yaml-cpp/include"
-    }
+	includedirs {
+		"res/",
+		"src/",
+		"kettle/kettle/src",
+		"kettle/thirdparty/imgui",
+		"thirdparty/yaml-cpp/include"
+	}
 
-    links {
-        "glfw",
-        "glad",
-        "imgui",
+	links {
+		"kettle",
 		"yaml-cpp"
-    }
+	}
 
-    files {
-        "src/**.h",
-        "src/**.cpp",
+	files {
+		"src/**.h",
+		"src/**.cpp",
+		"res/fa_icons.h", -- FontIcons
+	}
 
-        -- Resources
-        "res/fa_icons.h",
+	filter "Debug"
+		symbols "On"
 
-        -- ImGui files
-        "thirdparty/imgui/backends/imgui_impl_glfw.h",
-        "thirdparty/imgui/backends/imgui_impl_glfw.cpp",
-        "thirdparty/imgui/backends/imgui_impl_opengl3.h",
-        "thirdparty/imgui/backends/imgui_impl_opengl3.cpp"
-    }
+	filter "Release"
+		optimize "On"
 
-    filter "Debug"
-        defines { "DEBUG" }
-        symbols "On"
+	filter "system:windows"
+		systemversion "latest"
+		defines { "CP_WINDOWS" }
+		includedirs { "res/" }
+		files {
+			"res/coffeepot.rc",
+			"res/resources.h",
+			'res/**.ico'
+		}
 
-    filter "Release"
-        defines { "NDEBUG" }
-        optimize "On"
-
-    filter "system:windows"
-        systemversion "latest"
-        defines { "CP_WINDOWS", "YAML_CPP_STATIC_DEFINE" }
-
-        includedirs { "res/" }
-        files {
-            "res/coffeepot.rc",
-            "res/resources.h",
-            'res/**.ico'
-        }
-
-    filter "system:linux"
-        systemversion "latest"
-        links { "pthread", "dl" }
-        defines { "CP_LINUX" }
+	filter "system:linux"
+		systemversion "latest"
+		links { "pthread", "dl" }
+		links { "glad", "GLFW", "imgui" }
+		defines { "CP_LINUX" }
